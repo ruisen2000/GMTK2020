6e88d933620510c3ef19c8e7ea5f3df738e3376a
Isvan <iavorsivanov@gmail.com>
2020-07-11 13:48:53 -0700

    Fixed the import settings for the images, added frame work for cockpit mini game

---
+++ Assets/Scripts/CockpitMiniGame/CockpitMiniGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CockpitMiniGame : Minigame
+{
+    LineRenderer path;
+    ArrayList asteroids;
+
+
+    public GameObject asteroidPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void updateAsteroids()
+    {
+
+    }
+
+}
