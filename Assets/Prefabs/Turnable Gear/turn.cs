32aae4d89409bd3bb4e42d1d365353bcef7eccad
Greyson Wang <greysonw@sfu.ca>
2020-07-11 14:46:52 -0700

    Auto stash before merge of "master" and "origin/master"
    Add gears

---
+++ Assets/Prefabs/Turnable Gear/turn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turn : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        }
+
+
+    } 
+}
