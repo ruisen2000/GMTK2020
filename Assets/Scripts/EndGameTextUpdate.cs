1a6f63be052972e8ac52f4602a1c1b9ba7fe6467
Isvan <iavorsivanov@gmail.com>
2020-07-12 11:10:05 -0700

    I fix things hannah tell me to fix

--- Assets/Scripts/EndGameTextUpdate.cs
+++ Assets/Scripts/EndGameTextUpdate.cs
@@ -8,15 +8,25 @@ public class EndGameTextUpdate : MonoBehaviour
     [SerializeReference]
     Text distanceScore;
 
+    [SerializeReference]
+    Text hpLeft;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        distanceScore.text = DataHolder.allData.distanceTravelled.ToString("F2");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceScore.text = DataHolder.allData.distanceTravelled.ToString("F2") + "km";
+
+        if(hpLeft != null)
+        {
+            hpLeft.text = (DataHolder.allData.shipHealth * 100.0f).ToString("F2") + "% Hull";
+        }
+
     }
 }
